51dfaf4aeab32216439ab9e0f0f9b536f9087522
Venkat <kureti.venkat.nishit@gmail.com>
2026-05-14 14:17:54 +0530

    Removed Hardcoded Values

---
+++ terraform/dev.tfvars
@@ -0,0 +1,17 @@
+subscription_id = "47e11824-86bc-41f8-9dbd-2816b994a6a2"
+
+location = "South India"
+
+resource_group_name = "RG_cal_ind_dev_05"
+
+app_service_plan_name = "ASP_cal_ind_dev_05"
+
+web_app_name = "webcalinddev05"
+
+acr_name = "acrcalinddev05"
+
+aks_cluster_name = "aks-cal-ind-dev-05"
+
+aks_dns_prefix = "aks-cal-ind-dev-05"
+
+container_image_repository = "dragot-calculator"
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,13 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.webapp.default_hostname
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "webapp_name" {
+  value = azurerm_linux_web_app.webapp.name
+}
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
 }
 
 output "acr_login_server" {
@@ -8,4 +16,12 @@ output "acr_login_server" {
 
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "image_repository" {
+  value = var.container_image_repository
+}
+
+output "webapp_url" {
+  value = azurerm_linux_web_app.webapp.default_hostname
 }
